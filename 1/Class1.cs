13ab6bab8e1f9946ef2b3a2720109a1932475b24
Sergey Khromenkov <khromenkov00@bk.ru>
2024-12-21 01:35:45 +0200

    Добавьте файлы проекта.

---
+++ 1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace _1
+{
+    public class Mas1Op
+    {
+        public static int Summa(int[,] mas1 )
+        {
+            int summa2 = 0;
+            foreach (int element in mas1)
+        {
+            if(element % 3 == 0)
+        {
+            summa2 += element;
+        }
+            }
+        return summa2;
+        }
+    }
+}
---
+++ БИБЛИОТЕКИ/Program.cs
@@ -0,0 +1,29 @@
+﻿using _1;
+namespace БИБЛИОТЕКИ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 8; 
+            int m = 3; 
+            int[,] mas1 = new int[n, m];
+
+            Console.WriteLine("Введите элементы матрицы:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Строка {i + 1}: ");
+                string[] input = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    if (j < input.Length)
+                    {
+                        mas1[i, j] = int.Parse(input[j]);
+                    }
+                }
+            }
+            int result = Mas1Op.Summa(mas1);
+            Console.WriteLine($"Сумма элементов матрицы, кратных 3: {result}");
+        }
+    }
+}
